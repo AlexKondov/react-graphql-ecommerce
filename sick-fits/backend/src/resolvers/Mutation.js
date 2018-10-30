eab0b76d0e95269190148e0b60c68b22ab6cc444
Alex Kondov <alexander.kondov@resolvesys.com>
2018-10-30 16:21:27 +0200

    added user signup on the server

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const mutations = {
   async createItem(parent, args, context, info) {
     const item = await context.db.mutation.createItem(
@@ -34,6 +36,35 @@ const mutations = {
     // 2. check if they own the item
     // 3. delete it
     return context.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, context, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the db
+    const user = await context.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // We set the jwt as a cookie on the response
+    context.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+
+    //return the user
+    return user;
   }
 };
 
