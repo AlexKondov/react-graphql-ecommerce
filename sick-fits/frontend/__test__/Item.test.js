94b64ca3b7116cf4defec6fd7590499c2a2842e1
AlexKondov <alexkondov38@gmail.com>
2018-11-07 17:44:01 +0200

    added tests for the Item component and changed test config

---
+++ sick-fits/frontend/__test__/Item.test.js
@@ -0,0 +1,32 @@
+import ItemComponent from "../components/Item";
+import { shallow } from "enzyme";
+const fakeItem = {
+  id: "ABC123",
+  title: "Cool Item",
+  price: 5000,
+  description: "Cool Item Description",
+  image: "dog.jpg",
+  largeImage: "dog.jpg"
+};
+
+describe("<Item />", () => {
+  it("renders and displays properly", () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const PriceTag = wrapper.find("PriceTag");
+    expect(PriceTag.children().text()).toBe("$50");
+    expect(wrapper.find("Title a").text()).toBe("Cool Item");
+
+    const img = wrapper.find("img");
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+
+  it("renders out the buttons properly", () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const buttonList = wrapper.find(".buttonList");
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.filter("Link")).toBeTruthy();
+    expect(buttonList.filter("AddToCart")).toBeTruthy();
+    expect(buttonList.filter("DeleteItem")).toBeTruthy();
+  });
+});
--- sick-fits/frontend/__test__/formatMoney.test.js
+++ sick-fits/frontend/__test__/formatMoney.test.js
@@ -1,4 +1,4 @@
-import { formatMoney } from "../lib/formatMoney";
+import formatMoney from "../lib/formatMoney";
 
 describe("formatMoney function", () => {
   it("works with fractional dollars", () => {
