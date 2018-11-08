051b589739ac7395d603645c90c64032b8a4f50f
AlexKondov <alexkondov38@gmail.com>
2018-11-08 17:30:47 +0200

    added tests for sign in component and nav

---
+++ sick-fits/frontend/__test__/Nav.test.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { mount } from "enzyme";
+import wait from "waait";
+import Nav from "../components/Nav";
+import { CURRENT_USER_QUERY } from "../components/User";
+import { MockedProvider } from "react-apollo/test-utils";
+import { fakeUser, fakeCartItem } from "../lib/testUtils";
+import toJSON from "enzyme-to-json";
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } }
+  }
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } }
+  }
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [fakeCartItem(), fakeCartItem(), fakeCartItem()]
+        }
+      }
+    }
+  }
+];
+
+describe("<Nav />", () => {
+  it("renders a minimal nav when signed out", () => {
+    const wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+
+    const nav = wrapper.filter('ul[data-test="nav"]');
+    expect(toJSON(nav)).toMatchSnapshot();
+  });
+
+  it("renders full nav when signed in", () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(nav.children().length).toBe(6);
+    expect(nav.text()).toContain("Sign Out");
+    expect(nav.text()).toContain("Sell");
+  });
+
+  it("renders the amount of items in the cart", () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocksWithCartItems}>
+        <Nav />
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+
+    const nav = wrapper.find('ul[data-test="nav"]');
+    const count = nav.find("div.count");
+    expect(count).toMatchSnapshot;
+  });
+});
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -11,7 +11,7 @@ const Nav = () => {
   return (
     <User>
       {({ data: { me } }) => (
-        <NavStyles>
+        <NavStyles data-test="nav">
           <Link href="/items">
             <a>Shop</a>
           </Link>
